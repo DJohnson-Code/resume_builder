0a1bec6eeedb3e3d85e7553645eb7f27399fb020
Darnell Johnson <darnellharrisjohnson@gmail.com>
2025-09-16 20:42:39 -0500

    updated data model

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, HttpUrl, constr
+
+
+class ResumeIn(BaseModel):
+    name: str
+    email: EmailStr
+    number: constr(regex=r"^\+?\d[\d\s\-()]{9,14}$")
+    urls: list[HttpUrl] | None = None
+    experience: list[str] | None = None
+    skills: list[str]
+    education: list[str] | None = None
+    
+
+class ResumeOut(BaseModel): 
